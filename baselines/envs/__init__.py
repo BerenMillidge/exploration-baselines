f042cf0ef027f0c95d526808d1dfd6409cc06d30
Beren <bmillidgework@gmail.com>
2020-01-28 11:56:21 +0000

    make roboschool dependence optional

--- baselines/envs/__init__.py
+++ baselines/envs/__init__.py
@@ -1,8 +1,16 @@
 from .const import *
-from .mountain_car import SparseMountainCar
-from .cartpole_swingup import SparseCartpoleSwingup
-from .double_pendulum import SparseDoublePendulum
-from .half_cheetah import SparseHalfCheetah
-from .bipedal_walker import SparseBipedalWalker
+try:
+  import roboschool
+  from .mountain_car import SparseMountainCar
+  from .cartpole_swingup import SparseCartpoleSwingup
+  from .double_pendulum import SparseDoublePendulum
+  from .half_cheetah import SparseHalfCheetah
+  from .bipedal_walker import SparseBipedalWalker
+except:
+  print("Cannot import roboschool: only importing those envs which don't require it")
+  from .mountain_car import SparseMountainCar
+  from .double_pendulum import SparseDoublePendulum
+  from .bipedal_walker import SparseBipedalWalker
+  
 from .torch_env import TorchEnv
 from .wrappers import Wrapper, NoisyEnv
